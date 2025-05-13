b80cdaa5079e81c17258e7bec315071ee6bcde72
masaki8c8xmachioka <43851778+masaki8c8xmachioka@users.noreply.github.com>
2025-05-13 13:46:45 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,6 +46,6 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 ##st.text(fruityvice_response)
 ##st.text(fruityvice_response.json())
-st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+st_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
 
