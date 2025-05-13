469ced91e728aa1b95705ec93ef1123d707366a2
masaki8c8xmachioka <43851778+masaki8c8xmachioka@users.noreply.github.com>
2025-05-13 13:40:52 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,4 +44,5 @@ if ingred_list:
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+##st.text(fruityvice_response)
+st.text(fruityvice_response.json())
