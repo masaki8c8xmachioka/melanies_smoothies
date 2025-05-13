f779974f79f3a10f23188f17de483ee810f63012
masaki8c8xmachioka <43851778+masaki8c8xmachioka@users.noreply.github.com>
2025-05-13 14:51:05 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,10 +41,12 @@ if ingred_list:
         ingred_string += fruit_choosen + ' '
         
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         
         st.subheader(fruit_choosen + ' Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_choosen)
+        #smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_choosen)
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
+
         st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
         
     st.write(ingred_string)
